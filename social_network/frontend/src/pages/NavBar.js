369c6bfde7a86e8f4341815264f686880216ba79
Tinotenda Panashe Dauti <23171219@sun.ac.za>
2022-04-23 04:54:13 +0200

    partially complete navbar

--- social_network/frontend/src/pages/NavBar.js
+++ social_network/frontend/src/pages/NavBar.js
@@ -9,25 +9,34 @@ import {
   Button,
   IconButton,
   Container,
+  ButtonGroup
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
-
+import AccountIcon from "@material-ui/icons/AccountCircleTwoTone";
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 export default function NavBar() {
   return (
-    <Container maxWidth="sm">
-      <Box
-        sx={{
-          width: 300,
-          height: 300,
-          backgroundColor: 'primary.dark',
-          '&:hover': {
-            backgroundColor: 'primary.main',
-            opacity: [0.9, 0.8, 0.7],
-          },
-        }}
-      />
-    </Container>
+    <div className='nav-container'>
+     <AppBar color='secondary' position='sticky'>
+        <Toolbar disableGutters sx={{ order: 0, display: 'flex', position:'sticky' }}>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Link to="/login"><img className='logo' src='../../static/images/myapplogo.png' alt='logo'></img></Link>
+            </Box>
+              <ButtonGroup
+                size="large"
+                edge="flex-end"
+                color='inherit'
+                aria-label="open drawer"
+                positon="absolute"
+                sx={{ order: 5, justifyContent: 'flex-end', display: 'flex', flexDirection: 'row', alignItems: 'center', width: '20%' }}
+              >
+                <IconButton><AccountIcon color='dark_button'/></IconButton>
+                <IconButton><KeyboardArrowDownIcon color='dark_button' /></IconButton>
+              </ButtonGroup>
+       </Toolbar>
+     </AppBar>
+    </div>
   );
 }
 
